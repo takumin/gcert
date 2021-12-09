4fb34875587c23ad4b8debbfdfeb4ad2ce059adb
takumin <takumiiinn@gmail.com>
2021-12-10 08:34:26 +0900

    rename command parent directory

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/takumin/gcert/internal/app"
+	"github.com/urfave/cli/v2"
+
+	"github.com/takumin/gcert/internal/command/completion"
+	"github.com/takumin/gcert/internal/command/selfsign"
 	"github.com/takumin/gcert/internal/config"
 )
 
@@ -16,13 +20,23 @@ var (
 )
 
 func main() {
-	app := app.NewApp(
-		AppName,
-		Usage,
-		Version,
-		Revision,
-		config.NewConfig(),
-	)
+	config := config.NewConfig()
+
+	flags := []cli.Flag{}
+
+	cmds := []*cli.Command{}
+	cmds = append(cmds, completion.NewCommands(config, flags)...)
+	cmds = append(cmds, selfsign.NewCommands(config, flags)...)
+
+	app := &cli.App{
+		Name:                 AppName,
+		Usage:                Usage,
+		Version:              fmt.Sprintf("%s (%s)", Version, Revision),
+		Flags:                flags,
+		Commands:             cmds,
+		EnableBashCompletion: true,
+	}
+
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
